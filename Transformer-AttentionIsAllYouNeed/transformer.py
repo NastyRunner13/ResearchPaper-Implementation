c342b9b4e114c200490adcc85d0d817ee4b68db1
Prince Gupta <princegupta1586@gmail.com>
2024-10-12 17:39:43 +0530

    Add test funsction and fix errors

--- Transformer-AttentionIsAllYouNeed/transformer.py
+++ Transformer-AttentionIsAllYouNeed/transformer.py
@@ -30,14 +30,14 @@ class SelfAttention(nn.Module):
         keys = self.keys(keys)
         queries = self.queries(queries)
         
-        energy = torch.einsum("nqhd, nkhd-->nhqk", [queries, keys])
+        energy = torch.einsum("nqhd, nkhd->nhqk", [queries, keys])
 
         if mask is not None:
             energy = energy.masked_fill(mask==0, float("-1e20"))
 
         attention = torch.softmax(energy / (self.embed_size ** (1/2)), dim=3)
 
-        out = torch.einsum("nhql, nlhd-->nqhd", [attention, values]).reshape(
+        out = torch.einsum("nhql, nlhd->nqhd", [attention, values]).reshape(
             N, query_len, self.heads*self.head_dim
         )
 
@@ -166,7 +166,7 @@ class Decoder(nn.Module):
         x = self.dropout((self.word_embedding(x) + self.position_embedding(positions)))
 
         for layer in self.layers:
-            x = layer(x, enc_out, src_mask, trg_mask)
+            x = layer(x, enc_out, enc_out, src_mask, trg_mask)
         
         out = self.fc_out(x)
 
@@ -224,3 +224,18 @@ class Transformer(nn.Module):
         return out
     
 
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(device)
+    trg = torch.tensor([[1, 7, 4, 3, 5, 9, 2, 0], [1, 5, 6, 2, 4, 7, 6, 2]]).to(device)
+
+    src_pad_idx = 0
+    trg_pad_idx = 0
+    src_vocab_size = 10
+    trg_vocab_size = 10
+
+    model = Transformer(src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx).to(device)
+
+    out = model(x, trg[:, :-1])
+    print(out.shape)
