0c811b4cbea6fc4aaa91b3e5546c570100057972
Prince Gupta <princegupta1586@gmail.com>
2024-10-12 16:15:14 +0530

    Add DecoderBlock

--- Transformer-AttentionIsAllYouNeed/transformer.py
+++ Transformer-AttentionIsAllYouNeed/transformer.py
@@ -109,3 +109,25 @@ class Encoder(nn.Module):
             out = layer(out, out, out, mask)
 
         return out
+    
+
+class DecoderBlock(nn.Module):
+    
+    def __init__(self, embed_size, heads, forward_expansion, dropout, device):
+        super(DecoderBlock, self).__init__()
+
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm = nn.LayerNorm(embed_size)
+        self.transformer_block = TransformerBlock(
+            embed_size, heads, dropout, forward_expansion
+        )
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, value, key, src_mask, trg_mask):
+
+        attention = self.attention(x, x, x, trg_mask)
+        query = self.dropout(self.norm(attention + x))
+        
+        out = self.transformer_block(value, key, query, src_mask)
+        
+        return out
